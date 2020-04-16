10feeb65fd65ad80e47b298b7f4a8bbbae9ba62d
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-04-16 23:42:12 +0500

    Removed Dependencies

--- A_star_search.py
+++ A_star_search.py
@@ -4,7 +4,7 @@ from direction import Direction,Coordinate
 from board import Board
 
 class Node:
-    def __init__(self,board : Board, parent = None):
+    def __init__(self,board : list, parent = None):
         self.state = board
         self.parent = parent
         self.g_value = 0
@@ -15,12 +15,12 @@ class Node:
     def f(self):
         return self.g_value+self.h_value
 
-    def hamming_distance(self, state : Board, final: list):
+    def hamming_distance(self, state : list, final: list) -> int:
         cost = 0 
-        board_len = len(state.board)
+        board_len = len(state)
         for i in range(board_len):
             for j in range(board_len):
-                pos = self.get_pos(final, state.board[i][j])
+                pos = self.get_pos(final, state[i][j])
                 cost += abs(i - pos[0]) + abs(j - pos[1])
         return cost
 
@@ -49,13 +49,16 @@ class Node:
             x = inc_x + x
             y = inc_y + y
 
-            if game.is_valid_move(x,y, len(self.state.board)):
-                game.move(move, temp_node.state)
-                node = Node(temp_node.state, self)
-                list_nodes.append(node)
+            if game.is_valid_move(x,y, len(self.state)):
+                state_move = game.move(move, temp_node.state)
+
+                if state_move != None:
+                    temp_node.state = state_move
+                    node = Node(temp_node.state, self)
+                    list_nodes.append(node)
         return list_nodes
 
-def get_best_nodes(open_list):
+def get_best_nodes(open_list : dict):
     first_iter = True
     for node in open_list.values():
         if first_iter or node.f() < best_f:
@@ -64,24 +67,24 @@ def get_best_nodes(open_list):
             best_f = best_node.f()
     return best_node
 
-def a_star(start_state: Board):
-    open_list = { str(start_state.board) : Node(start_state)}
+def a_star(start_state: list):
+    open_list = { str(start_state.list) : Node(start_state)}
     closed_list = {}
 
     while len(open_list) > 0:
         exam_node = get_best_nodes(open_list)
-        closed_list[str(exam_node.state.board)] = exam_node
+        closed_list[str(exam_node.state)] = exam_node
 
-        if exam_node.state.board == game.win_state:
+        if exam_node.state == game.win_state:
             return (1,2,3)
         
         neighbors = exam_node.get_neighbors()
 
         for node in neighbors:
-            if str(node.state.board) in closed_list.keys() or str(node.state.board) in open_list.keys() and open_list[str(node.state.board)].f() < node.f():
+            if str(node.state) in closed_list.keys() or str(node.state) in open_list.keys() and open_list[str(node.state)].f() < node.f():
                 continue
-            open_list[str(node.state.board)] = node
-        del open_list[str(exam_node.state.board)]
+            open_list[str(node.state)] = node
+        del open_list[str(exam_node.state)]
     return None
 
 
